0b7ed074967127a3a505bfb4cc8d00abef8a244d
Eliza Li <elizal@andrew.cmu.edu>
2020-02-17 20:21:06 -0500

    routing error fixed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
-	belongs_to: category
-	validates_presence_of: title
-	validates_presence_of: content
+	belongs_to :category
+	validates_presence_of :title
+	validates_presence_of :content
 
 	scope :active, 	-> { where('active = ?', true) }
   	scope :alphabetical, -> { order('title') }
